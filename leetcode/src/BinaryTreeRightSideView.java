0784be1e45dc0a92310239a912914a61a05241cb
git4dkumaran <mail2dkumaran@gmail.com>
2017-01-07 21:25:32 -0800

    Update BinaryTreeRightSideView.java

--- leetcode/src/BinaryTreeRightSideView.java
+++ leetcode/src/BinaryTreeRightSideView.java
@@ -1,3 +1,18 @@
+/**
+Given a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom. For example, given the following binary tree,
+
+   1            <---
+ /   \
+2     3         <---
+ \     
+  5             <---
+You can see [1, 3, 5].
+
+Analysis
+
+This problem can be solve by using a queue. On each level of the tree, we add the right-most element to the results.
+**/
+
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
