b21527ca24a35dc39b66f49bd420e222e8d8b5c9
git4dkumaran <mail2dkumaran@gmail.com>
2017-01-07 21:27:06 -0800

    Update BinaryTreeVerticalOrderTraversal.java

--- leetcode/src/BinaryTreeVerticalOrderTraversal.java
+++ leetcode/src/BinaryTreeVerticalOrderTraversal.java
@@ -1,3 +1,7 @@
+/**
+Given a binary tree, return the vertical order traversal of its nodes' values. (ie, from top to bottom, column by column).
+**/
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
